e4b6fc0780dedbbbfae2d79c38d0241240550896
vinicozzi <vinicozzimincent@gmail.com>
2023-04-25 17:24:51 -0400

    idk

--- skeleton/phase_3_iteration.js
+++ skeleton/phase_3_iteration.js
@@ -1,28 +1,43 @@
-// Array.prototype.myEach = function(callback) {
-//     for (let i = 0; i < this.length; i++) {
-//       callback(this[i]);
+// // Array.prototype.myEach = function(callback) {
+// //     for (let i = 0; i < this.length; i++) {
+// //       callback(this[i]);
+// //     }
+// //   };
+
+// //   const myArray = [1, 2, 3, 4];
+
+
+// Array.prototype.bubbleSort = function(arr) {
+//     let sorted = false;
+//     // const dup = arr;
+//     while (!sorted) {
+//         sorted = true;
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]){
+//                 let prev = arr[i];
+//                 arr[i] = arr[i+1];
+//                 arr[i+1] = prev;
+//                 sorted = false;
+//             }
+//         }
 //     }
-//   };
+//     return arr;
+// };
 
-//   const myArray = [1, 2, 3, 4];
 
+// console.log(Array.prototype.bubbleSort([9,2,1,5,4,6,8,3,2,3,4,6,-1,2,-4,56,5,7,10]))
 
-Array.prototype.bubbleSort = function(arr) {
-    let sorted = false;
-    // const dup = arr;
-    while (!sorted) {
-        sorted = true;
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i + 1]){
-                let prev = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = prev;
-                sorted = false;
+Array.prototype.subStrings = function(arr) {
+        let substrings = [];
+
+        for (let start = 0; start < arr.length; start++) {
+            for (let last = 0; last < arr.length; last++) {
+                console.log(start)
+                console.log(last)
+                substrings.push(arr.slice([start,[last]]));
             }
         }
-    }
-    return arr;
+        return substrings;
 };
 
-
-console.log(Array.prototype.bubbleSort([9,2,1,5,4,6,8,3,2,3,4,6,-1,2,-4,56,5,7,10]))
+console.log(Array.prototype.subStrings(["jump"]))

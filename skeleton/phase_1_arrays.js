d7dd0ce9cc5127097086bff102fe6740bbb4a34b
vinicozzi <vinicozzimincent@gmail.com>
2023-04-25 13:50:56 -0400

    problem_2

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,15 +1,31 @@
 
-Array.prototype.uniq = function(arr) {
-    let array = [];
+// Array.prototype.uniq = function(arr) {
+//     let array = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!array.includes(arr[i])) {
+//             array.push(arr[i]);
+//         }
+            
+//     }
+//     return array;
+// }
+
+// console.log(Array.prototype.uniq([1,2,2,3,4]))
+
+Array.prototype.twoSum = function(arr) {
+
+    let array = []; 
 
     for (let i = 0; i < arr.length; i++) {
-        if (!array.includes(arr[i])) {
-            array.push(arr[i]);
+        for (let j = 0; j < arr.length; j++) {
+            if ((j > i) && (arr[i] + arr[j] === 0)) {
+                array.push([i,j]);
+            }
         }
-            
     }
     return array;
 }
 
-console.log(Array.prototype.uniq([1,2,2,3,4]))
+console.log(Array.prototype.twoSum([1,2,-2,3,-3]))
 
